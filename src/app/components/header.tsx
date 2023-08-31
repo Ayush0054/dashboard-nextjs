9315cac58862c1af0b28a8d5b31fb2fd8f7b1977
Ayush0054 <ayush5aprill@gmail.com>
2023-09-01 02:07:34 +0530

    Feature : user photo and name from oauth

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,11 +1,15 @@
+"use client"
 import React from 'react'
 import ToggleSwitch from './toggleSwitch'
-
+import { signIn, signOut, useSession } from "next-auth/react";
 function Header() {
+  const { data: session } = useSession();
   return (
    
            <div className=" flex items-center justify-start gap-4 mb-8">
-            <h1 className="font-bold">Hey NAME -</h1>
+            {session && session.user &&
+              <h1 className="font-bold">Hey {session.user.name} -</h1>
+            }
             <h1 className=" text-slate-400">here’s what’s happening </h1>
             <div>
               <ToggleSwitch />
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import logo from "public/assets/logo/logo.png";
 import saasden from "public/assets/logo/SaasDen.png";
 import pfp from "public/assets/icons/pfp.png";
@@ -9,6 +10,7 @@ import MessageIcon from "./icons/message";
 import BellIcon from "./icons/bell";
 import SearchIcon from "./icons/search";
 function Navbar() {
+  const { data: session } = useSession();
   return (
     <div className=" flex justify-between mb-2 p-2 bg-[#fcfcfc]">
       <div className=" flex items-center gap-1">
@@ -26,7 +28,10 @@ function Navbar() {
       <div className=" flex items-center justify-between gap-3">
           <MessageIcon/>
              <BellIcon/>
-        <Image src={pfp} alt=""/>
+             {
+     session && session.user &&
+               <Image src={session.user.image as any} width={10} height={10} className=" w-10 h-10 rounded-full " alt=""/>
+             }
       </div>
     </div>
   );
