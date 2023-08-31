7f29991aaf9c1cf49154b6d7dad063e2427c8492
Ayush0054 <ayush5aprill@gmail.com>
2023-09-01 02:08:08 +0530

    dependency update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfigs
+module.exports = {
+    images: {
+      domains: ['lh3.googleusercontent.com'],
+    },
+    nextConfig
+  }
